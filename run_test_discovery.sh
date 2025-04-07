c93f51b29acd49683053c556989cc75103ab3c29
Nick Tarallo <47985788+nicktarallo@users.noreply.github.com>
2025-04-07 16:37:54 -0400

    got v100 outputs

---
+++ run_test_discovery.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --time=0:20:00
+#SBATCH --job-name=Inference
+#SBATCH --mem=10G
+#SBATCH --output=v100_my_job_%j.out
+#SBATCH --error=v100_my_job_%j.err
+#SBATCH --gres=gpu:v100-sxm2:1
+#SBATCH --partition=gpu
+
+lscpu
+module load anaconda3/2022.05 cuda/11.7
+source activate pytorch_env
+echo "no amp"
+python testing_cifar10.py
+echo "amp"
+python testing_cifar10.py --use_amp
